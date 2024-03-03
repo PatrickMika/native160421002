ab37d8b3ca71e8e5888692459c6cecda0a6fb653
PatrickMika <s160421002@student.ubaya.ac.id>
2024-03-03 15:44:21 +0700

    Tugas Week 2

---
+++ app/src/main/java/com/nmp/adv160421002week2/GameFragment.kt
@@ -0,0 +1,27 @@
+package com.nmp.adv160421002week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nmp.adv160421002week2.databinding.FragmentGameBinding
+
+class GameFragment : Fragment() {
+    private lateinit var binding: FragmentGameBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (arguments != null) {
+            val playerName =
+                GameFragmentArgs.fromBundle(requireArguments()).playerName
+            binding.txtTurn.text = "$playerName's Turn"
+        }
+    }
+}
---
+++ app/src/main/java/com/nmp/adv160421002week2/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.nmp.adv160421002week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.nmp.adv160421002week2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
+}
